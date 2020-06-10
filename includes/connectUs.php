949e181a9956055edef92565600fa810a6d7fa27
lgngaming <adam+locke@adam-swanson.com>
2020-06-09 22:49:50 -0700

    last minute pre-launch changes

--- includes/connectUs.php
+++ includes/connectUs.php
@@ -7,12 +7,15 @@
 </div>
 <div class="connectCol">
 <h4>Twitter Feed</h4>
+<a class="twitter-timeline" data-width="400" data-height="500" data-dnt="true" data-theme="dark" href="https://twitter.com/lgn_community?ref_src=twsrc%5Etfw">Tweets by lgn_community</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
 </div>
 <div class="connectCol">
 <h4>Facebook Feed</h4>
+<p><i>Coming Soon</i></p>
 </div>
 <div class="connectCol">
 <h4>LGN Discord Chanel</h4>
+<p><i>Coming Soon</i></p>
 </div>
 </div>
 </div>
--- includes/head.php
+++ includes/head.php
@@ -11,7 +11,14 @@ include('assets/functions.php');
 <!DOCTYPE html>
   <body>
 <head>
-<title>Legion rules</title>
+<title>Team Legion Gaming Community - Welome</title>
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#d90000">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
